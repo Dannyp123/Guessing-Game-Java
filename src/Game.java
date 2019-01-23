fc1972e154db444fd7d159e6c03798bbcbd2a929
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-01-23 13:31:57 -0600

    Added a while loop to the game to keep the player to be able to keep guessing until the player guesses the right number.

--- src/Game.java
+++ src/Game.java
@@ -12,15 +12,20 @@ public class Game {
     }
     public void guessingTheNum() {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Pick a number from 1 to 10 >> ");
-        int num = reader.nextInt();
+        boolean n = true;
+        while (n == true) {
+            System.out.println("Pick a number from 1 to 10 >> ");
+            int num = reader.nextInt();
 
-        if(num == randNum) {
-            System.out.println("Great!");
+            if(num == randNum) {
+                System.out.println("You guessed right my friend!");
+                System.exit(0);
+            }
+            else {
+                System.out.println("Nope...guess more!");
+            }
         }
-        else {
-            System.out.println("Keep going!");
-        }
+
     }
 
 }
