3bcbc6e7a8afef4c3d93afb7b95e7ef6d60fb479
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-01-23 13:38:10 -0600

    Added messages that let the player know if they are either lower or higher than the target number. I did this by using if the num is lower or higher than the players num then say go up or go down.

--- src/Game.java
+++ src/Game.java
@@ -7,22 +7,26 @@ public class Game {
 
     public int makingRandomNumber() {
         Random randint = new Random();
-        int num = randint.nextInt(10) + 1;
+        int num = randint.nextInt(100) + 1;
         return num;
     }
     public void guessingTheNum() {
         Scanner reader = new Scanner(System.in);
         boolean n = true;
+
+        System.out.print("Pick a number from 1 to 100 ");
         while (n == true) {
-            System.out.println("Pick a number from 1 to 10 >> ");
             int num = reader.nextInt();
 
             if(num == randNum) {
                 System.out.println("You guessed right my friend!");
                 System.exit(0);
             }
-            else {
-                System.out.println("Nope...guess more!");
+            else if(num < randNum) {
+                System.out.println("Your are to low..Guess Higher!");
+            }
+            else if (num > randNum) {
+                System.out.println("You are to high..Guess Lower!");
             }
         }
 
